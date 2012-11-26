55de724bcb9397b8e6180f1eb178816880324119
Asakawa Ryu <ryua775@gmail.com>
2012-11-26 20:11:17 +0800

    add author name

--- downloader.rb
+++ downloader.rb
@@ -4,11 +4,12 @@ require 'open-uri'
 doc = Nokogiri::HTML open(ARGV[0])
 doc.css('span.song-title a').each do |link|
   file_name = link.attributes['title'].value
+  author = link.parent.next.next.children[1].children[1].attributes['title']
   path = link.attributes['href'].value
   download_page = Nokogiri::HTML open("http://music.baidu.com#{path}/download")
   download_button = download_page.css 'a#download'
   file_path = download_button[0].attributes['href'].value
   Thread.new do
-    `wget -O "#{file_name}".mp3 http://music.baidu.com#{file_path}`
+    `wget -O "#{file_name}-#{author}".mp3 http://music.baidu.com#{file_path}`
   end
 end
